18d70fdf5c5048fe8651192c8d0173dced603322
lauichan <te06230623@gmail.com>
2024-02-19 13:26:47 +0900

    [ADD] header 마이페이지, 로그아웃 버튼 추가

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,13 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./Layout.module.css";
+import { useDispatch, useSelector } from "react-redux";
+import { logOut } from "../../store/modules/auth";
 
 const Header = () => {
+  const dispatch = useDispatch();
+  const auth = useSelector((state) => state.auth);
+
+  const handleLogOut = () => {
+    dispatch(logOut());
+  };
+
   return (
     <header className={styles.header}>
       <h1>
         <Link to="/">에스파 팬레터 콜렉션</Link>
       </h1>
+      {auth ? (
+        <>
+          <Link className={styles.mypage} to="/profile">
+            마이페이지
+          </Link>
+          <button className={styles.logout} onClick={handleLogOut}>
+            로그아웃
+          </button>
+        </>
+      ) : (
+        ""
+      )}
     </header>
   );
 };
