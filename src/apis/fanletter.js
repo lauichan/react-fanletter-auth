189dfe94a655119ba06813aa5627e251555a9ad7
lauichan <te06230623@gmail.com>
2024-02-19 22:33:41 +0900

    [ADD] fanletter API 파일

---
+++ src/apis/fanletter.js
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+const fanLetterAPI = axios.create({
+  baseURL: import.meta.env.VITE_APP_SERVER_URL,
+});
+
+fanLetterAPI.interceptors.request.use(
+  function (config) {
+    console.log("요청 성공", config);
+    return config;
+  },
+  function (error) {
+    console.log("요청 오류", error);
+    return Promise.reject(error);
+  }
+);
+
+fanLetterAPI.interceptors.response.use(
+  function (response) {
+    console.log("응답 성공", response);
+    return response;
+  },
+
+  function (error) {
+    console.log("응답 오류", error);
+    return Promise.reject(error);
+  }
+);
+
+export default fanLetterAPI;
