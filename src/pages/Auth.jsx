a769a23caf69f6318222f968b8ac963a8a6b8c6e
lauichan <te06230623@gmail.com>
2024-02-20 10:59:32 +0900

    [ADD] 로그인사용자는 로그인페이지에서 홈으로

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,14 +1,18 @@
 import { useState } from "react";
 import SignUp from "../components/auth/SignUp";
 import LogIn from "../components/auth/Login";
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
 
 function Auth() {
   const [toggle, setToggle] = useState(false);
+  const user = useSelector((state) => state.auth.user);
 
   const handleToggle = () => {
     setToggle(!toggle);
   };
 
+  if (user) return <Navigate to="/" />;
   if (toggle) return <SignUp handleToggle={handleToggle} />;
 
   return <LogIn handleToggle={handleToggle} />;
