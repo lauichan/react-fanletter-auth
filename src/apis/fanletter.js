10efa757659f90a441d77b3da90264913cea7ed7
lauichan <te06230623@gmail.com>
2024-02-21 23:02:30 +0900

    [ADD] 디버깅용

--- src/apis/fanletter.js
+++ src/apis/fanletter.js
@@ -4,4 +4,14 @@ const fanLetterAPI = axios.create({
   baseURL: import.meta.env.VITE_APP_SERVER_URL,
 });
 
+fanLetterAPI.interceptors.request.use(
+  function (config) {
+    return config;
+  },
+  function (error) {
+    console.error("요청 오류", error.response.data.message);
+    return Promise.reject(error);
+  }
+);
+
 export default fanLetterAPI;
