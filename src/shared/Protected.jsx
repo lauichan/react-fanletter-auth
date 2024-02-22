8de22002201b5e8d3f47ba5cf5f2f6700d317e1e
lauichan <te06230623@gmail.com>
2024-02-22 10:57:50 +0900

    [MOD] 팬레터 데이터를 엑세스 토큰이 있을때만 요청

--- src/shared/Protected.jsx
+++ src/shared/Protected.jsx
@@ -1,12 +1,20 @@
-import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router-dom";
+import { __getFanLetter } from "../store/modules/fanletter";
 
 function Protected() {
   const { user, isError } = useSelector((state) => state.auth);
 
+  const dispatch = useDispatch();
+
   if (!user?.accessToken || isError) {
     return <Navigate to="/auth" />;
   }
+
+  useEffect(() => {
+    dispatch(__getFanLetter());
+  }, [dispatch]);
 
   return <Outlet />;
 }
