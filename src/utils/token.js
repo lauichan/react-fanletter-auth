f8870cf5a6f18793313605e2b0c2045911fd2d7c
lauichan <te06230623@gmail.com>
2024-02-22 11:26:38 +0900

    [ADD] 토큰 만료 메시지

--- src/utils/token.js
+++ src/utils/token.js
@@ -2,11 +2,11 @@ import authAPI from "../apis/auth";
 
 export const isValidToken = async () => {
   try {
-    const response = await authAPI.get("/user");
-    console.log("토큰 살아있음", response);
+    await authAPI.get("/user");
   } catch (err) {
-    console.log(err);
+    console.error(err);
     localStorage.clear();
+    alert("토큰이 만료되었습니다. 다시 로그인 해주세요");
     return false;
   }
   return true;
